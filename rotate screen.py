40c772337b8c77001e329ab9c46d081acc5c9f5e
kash_secret <andersonkash212@gmail.com>
2024-10-19 18:00:00 -0500

    Add files via upload
    Signed-off-by: kash_secret <andersonkash212@gmail.com>

---
+++ rotate screen.py
@@ -0,0 +1,11 @@
+import rotatescreen
+import time
+
+screen = rotatescreen.get_primary_display()
+start_pos = screen.current_orientation
+
+for i in range(1,3):
+    pos = abs((start_pos - i*90) % 360)
+    screen.rotate_to(pos)
+    time.sleep(1.5)
+   
